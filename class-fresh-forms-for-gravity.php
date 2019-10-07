04020a59915a2c084c0157a4c093efdef4855f0a
samuelaguilera <correo@samuelaguilera.com>
2019-10-07 15:51:16 +0200

    Initial commit

---
+++ class-fresh-forms-for-gravity.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * Fresh Forms for Gravity Add-On.
+ *
+ * @since     1.0
+ * @package Fresh Forms for Gravity
+ * @author    Samuel Aguilera
+ * @copyright Copyright (c) 2019 Samuel Aguilera
+ */
+
+GFForms::include_addon_framework();
+
+/**
+ * Class Fresh_Forms_For_Gravity
+ *
+ * Primary class to manage the Fresh Forms for Gravity Add-On.
+ *
+ * @since 1.0
+ *
+ * @uses GFAddOn
+ */
+class Fresh_Forms_For_Gravity extends GFAddOn {
+
+	protected $_version                  = FRESH_FORMS_FOR_GRAVITY_VERSION;
+	protected $_min_gravityforms_version = '1.9';
+	protected $_slug                     = 'fresh_forms';
+	protected $_path                     = 'fresh-forms-for-gravity/fresh-forms-for-gravity.php';
+	protected $_full_path                = __FILE__;
+	protected $_title                    = 'Fresh Forms for Gravity';
+	protected $_short_title              = 'Fresh Forms';
+
+	private static $_instance = null;
+
+	/**
+	 * Get an instance of this class.
+	 *
+	 * @return Fresh_Forms_For_Gravity
+	 */
+	public static function get_instance() {
+		if ( null === self::$_instance ) {
+			self::$_instance = new Fresh_Forms_For_Gravity();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Handles hooks.
+	 */
+	public function init() {
+		parent::init();
+		// Let's see if we need to stop caching.
+		add_filter( 'template_redirect', array( $this, 'donotcache_and_headers' ) );
+
+		// WP Fastest Cache doesn't support DONOTCACHEPAGE ...
+		if ( class_exists( 'WpFastestCache' ) ) {
+			add_filter( 'wp_footer', array( $this, 'wpfc_blockCache' ) );
+		}
+
+		// I could check for the CloudFlare plugin, but many people is using CloudFlare without having the plugin installed.
+		add_filter( 'script_loader_tag', 'rocket_loader_exclude_gf_scripts', 10, 3 );
+
+		/**
+		 * Exclude Gravity Forms scripts from Rocket Loader minification. All Gravity Forms scripts are already minified.
+		 *
+		 * @param string $tag    The <script> tag for the enqueued script.
+		 * @param string $handle The script's registered handle.
+		 * @param string $src    The script's source URL.
+		 */
+		function rocket_loader_exclude_gf_scripts( $tag, $handle, $src ) {
+			if ( strpos( $handle, 'gform' ) !== false || 'plupload-all' === $handle ) {
+				return str_replace( "src='", "data-cfasync='false' src='", $tag );
+			} else {
+				return $tag;
+			}
+		}
+
+		// All Gravity Forms scripts are already minified.
+		add_filter( 'sgo_js_minify_exclude', 'sgo_exclude_gf_scripts' );
+		// Current branch of Gravity Forms (2.4), by default, doesn't support async loading of scripts.
+		add_filter( 'sgo_js_async_exclude', 'sgo_exclude_gf_scripts' );
+
+		/**
+		 * Exclude Gravity Forms scripts from SGO minification and async loading.
+		 *
+		 * @param array $exclude_list List of script handlers to exclude.
+		 */
+		function sgo_exclude_gf_scripts( $exclude_list ) {
+
+			$exclude_list[] = 'gform_conditional_logic';
+			$exclude_list[] = 'gform_datepicker_init';
+			$exclude_list[] = 'plupload-all';
+			$exclude_list[] = 'gform_json';
+			$exclude_list[] = 'gform_gravityforms';
+			$exclude_list[] = 'gform_textarea_counter';
+			$exclude_list[] = 'gform_masked_input';
+			$exclude_list[] = 'gform_chosen';
+			$exclude_list[] = 'gform_placeholder';
+
+			return $exclude_list;
+		}
+	}
+
+	/**
+	 * Check if post/page has a GF shortcode or block.
+	 *
+	 * @param integer $post_id      ID of the post.
+	 * @param string  $post_content Post body.
+	 */
+	public function has_gf( $post_id, $post_content ) {
+
+		// Setting initial values for the $has_gf array.
+		$has_gf = array(
+			'shortcode' => false,
+			'block'     => false,
+		);
+
+		// Check for a GF shortcode.
+		if ( has_shortcode( $post_content, 'gravityform' ) ) {
+			// Shortcode found!
+			$has_gf['shortcode'] = 'yes';
+			$this->log_debug( __METHOD__ . '(): GF shortcode detected in post ID ' . $post_id );
+		}
+
+		// Check for a GF block.
+		if ( function_exists( 'has_block' ) ) {
+
+			$gf_blocks = array( 'gravityforms/block', 'gravityforms/mailchimp', 'gravityforms/form' );
+
+			foreach ( $gf_blocks as $gf_block ) {
+
+				if ( has_block( $gf_block, $post_content ) ) {
+
+					// Block found!
+					$has_gf['block'] = 'yes';
+					$this->log_debug( __METHOD__ . '(): GF block detected in post ID ' . $post_id );
+
+					// No need to keep running.
+					break;
+				}
+			}
+		}
+
+		// Return array with results.
+		return $has_gf;
+
+	}
+
+	/**
+	 * Check if we're in a post/page and has the shortcode or block.
+	 *
+	 * @param integer $post_id      ID of the post.
+	 */
+	public function maybe_no_cache( $post_id ) {
+
+		// Running only for posts (any type) and pages.
+		if ( ! is_single( $post_id ) && ! is_page( $post_id ) ) {
+			$this->log_debug( __METHOD__ . '(): Not single post or page.' );
+			return false;
+		}
+
+		$this->log_debug( __METHOD__ . '(): Calling has_gf() for post ID ' . $post_id );
+		$has_gf = $this->has_gf( $post_id, $post->post_content );
+
+		// No shortcode and no block? Do nothing.
+		if ( ! in_array( 'yes', $has_gf, true ) ) {
+			$this->log_debug( __METHOD__ . '(): No form found, nothing to do...' );
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 *  Prevent caching if a GF shortcode or block is embedded in the post/page.
+	 */
+	public function donotcache_and_headers() {
+
+		global $post;
+
+		$has_gf = $this->maybe_no_cache( $post->ID );
+
+		// No shortcode and no block? Do nothing.
+		if ( false === $has_gf ) {
+			return;
+		}
+
+		// At this point we have found a shortcode or block, fresh time!
+		$this->log_debug( __METHOD__ . '(): Keep it fresh!' );
+
+		// Delete existing cache? No. Why? If the page is cached no PHP will be executed for the page, so we can't do anything.
+
+		// Prevent post (currently not cached) to be cached by plugins.
+		if ( ! defined( 'DONOTCACHEPAGE' ) ) {
+			define( 'DONOTCACHEPAGE', true );
+		}
+
+		// Prevent object caching.
+		if ( ! defined( 'DONOTCACHEOBJECT' ) ) {
+			define( 'DONOTCACHEOBJECT', true );
+		}
+
+		// Prevent database caching.
+		if ( ! defined( 'DONOTCACHEDB' ) ) {
+			define( 'DONOTCACHEDB', true );
+		}
+
+		// LiteSpeed Page Cache.
+		if ( ! defined( 'LSCACHE_NO_CACHE' ) ) {
+			define( 'LSCACHE_NO_CACHE', true );
+		}
+		// LiteSpeed Object Cache.
+		if ( ! defined( 'LSCWP_OBJECT_CACHE' ) ) {
+			define( 'LSCWP_OBJECT_CACHE', false );
+		}
+
+		// Sets the nocache headers to prevent caching by browsers and proxies respecting these headers.
+		nocache_headers();
+		// Adding no-store value to Cache-Control header for additional enforcement.
+		header( 'Cache-Control: no-store', false );
+
+	}
+
+	/**
+	 *  Prevent caching for WP Fastest Cache.
+	 */
+	public function wpfc_blockCache() {
+
+		global $post;
+
+		$has_gf = $this->maybe_no_cache( $post->ID );
+
+		// No shortcode and no block? Do nothing.
+		if ( false === $has_gf ) {
+			return;
+		}
+
+		echo '<!-- [wpfcNOT] -->';
+		$this->log_debug( __METHOD__ . '(): Added [wpfcNOT] for WP Fastest Cache.' );
+	}
+
+}
---
+++ fresh-forms-for-gravity.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Plugin Name: Fresh Forms for Gravity
+ * Plugin URI: http://www.samuelaguilera.com
+ * Description: Prevent posts and pages with a Gravity Forms shortcode or Gutenberg block from being cached.
+ * Author: Samuel Aguilera
+ * Version: 1.0
+ * Author URI: http://www.samuelaguilera.com
+ * Text Domain: fresh-forms-for-gravity
+ * Domain Path: /languages
+ * License: GPL3
+ *
+ * @package Fresh Forms for Gravity
+ */
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+define( 'FRESH_FORMS_FOR_GRAVITY_VERSION', '1.0' );
+
+add_action( 'gform_loaded', array( 'Fresh_Forms_For_Gravity_Bootstrap', 'load' ), 5 );
+register_activation_hook( __FILE__, 'sar_purge_all_cache' );
+
+/**
+ * Class Fresh_Forms_For_Gravity_Bootstrap
+ *
+ * Handles the loading of the Add-On and registers with the Add-On framework.
+ *
+ * @since 1.0.0
+ */
+class Fresh_Forms_For_Gravity_Bootstrap {
+
+	/**
+	 * If the Add-On Framework exists, Fresh Forms for Gravity Add-On is loaded.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @uses GFAddOn::register()
+	 *
+	 * @return void
+	 */
+	public static function load() {
+
+		if ( ! method_exists( 'GFForms', 'include_addon_framework' ) ) {
+			return;
+		}
+
+		require_once 'class-fresh-forms-for-gravity.php';
+
+		GFAddOn::register( 'Fresh_Forms_For_Gravity' );
+	}
+
+}
+
+/**
+ * Obtains and returns an instance of the SAR_Fresh_Forms
+ *
+ * @since  1.0.0
+ * @access public
+ *
+ * @uses Fresh_Forms_For_Gravity::get_instance()
+ *
+ * @return object Fresh_Forms_For_Gravity
+ */
+function fresh_forms_for_gravity() {
+	return Fresh_Forms_For_Gravity::get_instance();
+}
+
+/**
+ * Purge everything for a fresh start... This is required in order to allow donotcache_and_headers() to run before caching a page.
+ */
+function sar_purge_all_cache() {
+
+	// W3TC.
+	if ( function_exists( 'w3tc_pgcache_flush' ) ) {
+		// Delete page cache.
+		w3tc_pgcache_flush();
+	}
+
+	// WP Super Cache.
+	if ( function_exists( 'wp_cache_clean_cache' ) ) {
+		global $file_prefix;
+
+		empty( $file_prefix ) ? $file_prefix = 'wp-cache-' : $file_prefix;
+		wp_cache_clean_cache( $file_prefix, true );
+	}
+
+	// WP Fastest Cache.
+	if ( class_exists( 'WpFastestCache' ) ) {
+		$wpfc = new WpFastestCache();
+		$wpfc->deleteCache();
+	}
+
+	// WP Rocket.
+	if ( function_exists( 'rocket_clean_domain' ) ) {
+		rocket_clean_domain();
+	}
+
+	// Autoptimize.
+	if ( class_exists( 'autoptimizeCache' ) ) {
+		autoptimizeCache::clearall();
+	}
+
+	// SG Optimizer.
+	if ( class_exists( 'Supercacher' ) ) {
+		Supercacher::purge_everything();
+	}
+
+	// Comet Cache.
+	if ( class_exists( 'comet_cache' ) ) {
+		comet_cache::clear();
+	}
+
+	// LiteSpeed Cache.
+	if ( class_exists( 'LiteSpeed_Cache_Purge' ) ) {
+		LiteSpeed_Cache_Purge::all();
+	}
+
+	// Hummingbird.
+	if ( class_exists( 'Hummingbird\Core\Filesystem' ) ) {
+		// I would use Hummingbird\WP_Hummingbird::flush_cache( true, false ) instead, but it's disabling the page cache option in Hummingbird settings.
+		Hummingbird\Core\Filesystem::instance()->clean_up();
+	}
+
+	// WP Optimize.
+	if ( class_exists( 'WP_Optimize_Cache_Commands' ) ) {
+		// This function returns a response, so I'm assigning it to a variable to prevent unexpected output to the screen.
+		$response = WP_Optimize_Cache_Commands::purge_page_cache();
+	}
+
+}
